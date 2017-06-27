d9761534f3115f5ba8967517d722ded56fafa751
Carl Van Schie <carl.vanschie@gmail.com>
2017-06-27 21:20:42 +0100

    Multiplier to eureka.

--- multiply/src/main/java/app/Application.java
+++ multiply/src/main/java/app/Application.java
@@ -2,11 +2,13 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by carl on 23/06/17.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
