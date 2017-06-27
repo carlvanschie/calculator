fb70d29fbfa8850f01e64ef05aeaa7043a14abb5
Carl Van Schie <carl.vanschie@gmail.com>
2017-06-27 20:44:54 +0100

    Eureka server.

---
+++ euerka/src/main/java/vanschie/Application.java
@@ -0,0 +1,18 @@
+package vanschie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by carl on 27/06/17.
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
