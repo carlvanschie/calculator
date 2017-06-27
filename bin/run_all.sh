49db6eccf3004285a4c3550be15636ed57b698fc
Carl Van Schie <carl.vanschie@gmail.com>
2017-06-27 22:07:39 +0100

    Updating euerka service to update...
    - putting ports through in command line.

--- bin/run_all.sh
+++ bin/run_all.sh
@@ -2,22 +2,16 @@
 
 
 # Eureka Server: 8761
-java -jar euerka/target/euerka-1.0-SNAPSHOT.jar
+java -jar euerka/target/euerka-1.0-SNAPSHOT.jar &
 
-# Services
-# ADD: 10001
-# SUB: 10002
-# MUL: 10003
-# DIV: 10004
-# SQU: 10005
-# POW: 10006
-# MOD: 10007
-# Batch 20001
-java -jar add/target/add-1.0-SNAPSHOT.jar &
-java -jar divide/target/divide-1.0-SNAPSHOT.jar &
-java -jar multiply/target/multiply-1.0-SNAPSHOT.jar &
-java -jar subtract/target/subtract-1.0-SNAPSHOT.jar &
-java -jar mod/target/mod-1.0-SNAPSHOT.jar &
-java -jar square/target/square-1.0-SNAPSHOT.jar &
-java -jar power/target/power-1.0-SNAPSHOT.jar &
-java -jar batchcalc/target/batch-calc-1.0-SNAPSHOT.jar &
+# Pause...
+sleep 20s
+
+java -jar -Dserver.port=10001 add/target/add-1.0-SNAPSHOT.jar &
+java -jar -Dserver.port=10002 divide/target/divide-1.0-SNAPSHOT.jar &
+java -jar -Dserver.port=10003 multiply/target/multiply-1.0-SNAPSHOT.jar &
+java -jar -Dserver.port=10004 subtract/target/subtract-1.0-SNAPSHOT.jar &
+java -jar -Dserver.port=10005 mod/target/mod-1.0-SNAPSHOT.jar &
+java -jar -Dserver.port=10006 square/target/square-1.0-SNAPSHOT.jar &
+java -jar -Dserver.port=10007 power/target/power-1.0-SNAPSHOT.jar &
+java -jar -Dserver.port=10008 batchcalc/target/batch-calc-1.0-SNAPSHOT.jar &
