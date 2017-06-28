9fb1fc3f5fa913ba5a64aea4c950c733ffc6e93d
Carl Van Schie <carl.vanschie@gmail.com>
2017-06-28 20:03:18 +0100

    Adding of docker build script.

---
+++ bin/docker/build_docker.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+echo "Installing Clients"
+cd feign_clients/
+mvn clean install
+
+echo "Packaging Add"
+cd ../add/
+mvn clean package
+
+echo "Packaging Subtract"
+cd ../subtract/
+mvn clean package docker:build
+
+echo "Packaging Divide"
+cd ../divide/
+mvn clean package docker:build
+
+echo "Packaging Multiply"
+cd ../multiply/
+mvn clean package docker:build
+
+echo "Packaging Mod"
+cd ../mod/
+mvn clean package docker:build
+
+echo "Packaging Power"
+cd ../power/
+mvn clean package docker:build
+
+echo "Packaging Square"
+cd ../square/
+mvn clean package docker:build
+
+echo "Packaging Square"
+cd ../batchcalc/
+mvn clean package docker:build
+
+echo "Eureka Server"
+cd ../euerka/
+mvn clean package docker:build
