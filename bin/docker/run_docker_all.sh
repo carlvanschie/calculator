9401b314b53d6ef7ed57f8a0d8ed89552e8b92c1
Carl Van Schie <carl.vanschie@gmail.com>
2017-06-29 20:12:38 +0100

    Putting in the consul divide.

--- bin/docker/run_docker_all.sh
+++ bin/docker/run_docker_all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
-docker run --net=host -d vanschie/euerka
+docker run -p 8300:8300 -p 8400:8400 -p 8500:8500 -p 8600:8600 \
+    -v /data -v /config -d progrium/consul -server -bootstrap -data-dir /data
 
 docker run -e SERVER_PORT=10101 --net=host -d vanschie/add
 docker run -e SERVER_PORT=10201 --net=host -d vanschie/divide
@@ -10,8 +10,6 @@ docker run -e SERVER_PORT=10401 --net=host -d vanschie/subtract
 docker run -e SERVER_PORT=10501 --net=host -d vanschie/mod
 docker run -e SERVER_PORT=10601 --net=host -d vanschie/square
 docker run -e SERVER_PORT=10701 --net=host -d vanschie/power
-
 docker run -e SERVER_PORT=10801 --net=host -d vanschie/batch-calc
 
 
-# docker run -p 8300:8300 -p 8400:8400 -p 8500:8500 -p 8600:8600 -v /data -v /config progrium/consul -server -bootstrap -data-dir /data
